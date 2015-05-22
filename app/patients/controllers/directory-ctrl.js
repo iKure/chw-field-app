5c56af84ead03121f3f371bd77f4ff940cce46d8
Nick Reid <nickreid@nickreid.com>
2015-05-22 12:09:08 +0530

    made clearing issue go away

--- app/patients/controllers/directory-ctrl.js
+++ app/patients/controllers/directory-ctrl.js
@@ -3,11 +3,11 @@ angular.module('patients')
 .controller('DirectoryCtrl', [ '$scope', 'Patients', function ($scope, Patients) {
   console.log('Hello from your Controller: DirectoryCtrl in module patients:. This is your controller:', this);
   var directory = this;
+  directory.patients = Patients.patients;
 
   $scope.$on('patients.update', function () {
     directory.patients = Patients.patients;
+    $scope.$apply();
+    console.log("DirectoryCtrl got patients update" + Patients.patients);
   });
-
-  this.patients = Patients.patients;
-
 }]);
