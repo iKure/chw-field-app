266ca363436ed1d17b867808c720d933752e2915
Nick Reid <nickreid@nickreid.com>
2015-05-23 13:26:31 +0530

    tweaked state routing

--- app/patients/patients.js
+++ app/patients/patients.js
@@ -11,16 +11,16 @@ angular.module('patients', [
 
   // some basic routing
   $stateProvider
-    .state('patients.directory', {
-      url: '/patients',
+    .state('patients_directory', {
+      url: '/directory',
       templateUrl: 'patients/templates/list.html',
     })
-    .state('patients.profile', {
+    .state('patients_profile', {
       url: '/profile/:id',
       templateUrl: 'patients/templates/profile.html',
     })
-    .state('patient.register', {
-      url: '/patients/register',
+    .state('patients_register', {
+      url: '/register',
       templateUrl: 'patients/templates/register.html',
       controller: 'RegisterCrtl as register'
     });
