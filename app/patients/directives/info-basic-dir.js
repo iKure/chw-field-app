5aa4e901f9f14640ca7bc55f250509742c03f6f0
Nick Reid <nickreid@nickreid.com>
2015-05-25 11:18:06 +0530

    added scope property to directive

--- app/patients/directives/info-basic-dir.js
+++ app/patients/directives/info-basic-dir.js
@@ -1,8 +1,13 @@
 'use strict';
 angular.module('patients')
-.directive('patinfo', function () {
+.directive('patinfo', ['Patients', function ($scope, Patients) {
+  var form = this;
+
   return {
+    scope:{
+      patient: '=patient',
+    },
     templateUrl: 'patients/templates/information-basic.html',
     restrict: 'E'
   };
-});
+}]);
