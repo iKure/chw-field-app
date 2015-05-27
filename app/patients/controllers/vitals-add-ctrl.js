ed275cdf8b9f205358e14f2691e2c653bd3a2504
Nick Reid <nickreid@nickreid.com>
2015-05-27 15:45:53 +0530

    cleared many functions out of vitals add ctrl

--- app/patients/controllers/vitals-add-ctrl.js
+++ app/patients/controllers/vitals-add-ctrl.js
@@ -1,40 +1,16 @@
 'use strict';
 angular.module('patients')
-.controller('VitalsAddCtrl', [ '$scope', '$state', '$stateParams', 'Patients', 'Vitals', function ($scope, $state, $stateParams, Patients, Vitals) {
+.controller('VitalsAddCtrl', [ '$scope', 'Vitals', function ($scope, Vitals) {
   console.log('Hello from your Controller: VitalsCtrl in module patients:. This is your controller:', this);
   var ctrl = this;
-  ctrl.records = Vitals.records;
-  ctrl.current = {};
-  ctrl.currentSet = {};
-
-  this.patient = Patients.current;
-
-  $scope.$on('vitals.update', function () {
-    ctrl.records = Vitals.records;
-    $scope.$apply();
-  });
-  $scope.$on('vitals.current.update', function () {
-    ctrl.current = Vitals.current;
-    if (ctrl.current.type) {
-      ctrl.currentSet[ctrl.current.type] = ctrl.current._id;
-    }
-    $scope.$apply();
-  });
+  Vitals.clearCurrentSet();
 
   function isSaved (type) {
-    if (ctrl.currentSet[type]) {
+    if (Vitals.getCurrentSet(type)) {
       return true;
     }
     return false;
   }
   this.isSaved = isSaved;
-
-  function isActive(type) {
-    if (ctrl.type == type) {
-      return true;
-    }
-    return false;
-  }
-  this.isActive = isActive;
 
 }]);
