9da83c8a3803e600ae5bea33aa035d9a4677e5c8
Nick Reid <nickreid@nickreid.com>
2015-05-28 08:11:30 +0530

    changed homepage to patient directory

--- app/main/main.js
+++ app/main/main.js
@@ -9,7 +9,7 @@ angular.module('main', [
 
   console.log('Allo! Allo from your module: ' + 'main');
 
-  $urlRouterProvider.otherwise('/tab/main');
+  $urlRouterProvider.otherwise('/tab/directory');
 
   // some basic routing
   $stateProvider
@@ -17,15 +17,6 @@ angular.module('main', [
       url: '/tab',
       abstract: true,
       templateUrl: 'main/templates/tabs.html'
-    })
-    .state('tabs.main', {
-      url: '/main',
-      views: {
-        'tabs-main': {
-          templateUrl: 'main/templates/start.html',
-          controller: 'StartCtrl as start'
-        }
-      }
     })
     .state('tabs.directory', {
       url: '/directory',
