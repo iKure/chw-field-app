5abdd8267f7009ab84f17c61114fa549cb6d757d
Nick Reid <nickreid@nickreid.com>
2015-05-28 23:41:15 +0530

    started to use $scope not this variable

--- app/patients/controllers/patient-summary-ctrl.js
+++ app/patients/controllers/patient-summary-ctrl.js
@@ -3,17 +3,15 @@ angular.module('patients')
 .controller('PatientSummaryCtrl', ['$scope', 'Patients', 'Vitals', function ($scope, Patients, Vitals) {
 
   console.log('Hello from your Controller: PatientSummaryCtrl in module patients:. This is your controller:', this);
-  var ctrl = this;
-  ctrl.patient = Patients.getCurrent();
-
+  
   $scope.$on('patient.change', function () {
-    ctrl.patient = Patients.getCurrent();
+    $scope.patient = Patients.getCurrent();
     $scope.$apply();
   });
 
-  ctrl.records = Vitals.get();
+  $scope.records = Vitals.get();
   $scope.$on('vitals.update', function () {
-    ctrl.records = Vitals.get();
+    $scope.records = Vitals.get();
     $scope.$apply();
   });
 }]);
