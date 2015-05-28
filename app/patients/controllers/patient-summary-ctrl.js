4d59ca222238cfc559778ed3a0d359d931cb0f3b
Nick Reid <nickreid@nickreid.com>
2015-05-28 23:42:14 +0530

    removed extra whitespace

--- app/patients/controllers/patient-summary-ctrl.js
+++ app/patients/controllers/patient-summary-ctrl.js
@@ -3,7 +3,7 @@ angular.module('patients')
 .controller('PatientSummaryCtrl', ['$scope', 'Patients', 'Vitals', function ($scope, Patients, Vitals) {
 
   console.log('Hello from your Controller: PatientSummaryCtrl in module patients:. This is your controller:', this);
-  
+
   $scope.$on('patient.change', function () {
     $scope.patient = Patients.getCurrent();
     $scope.$apply();
