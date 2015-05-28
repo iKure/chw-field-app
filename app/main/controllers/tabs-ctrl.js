9373202eab2d8be388f575066ceedbbfd0f81a47
Nick Reid <nickreid@nickreid.com>
2015-05-28 21:12:28 +0530

    added hook to catch non-tabs state

--- app/main/controllers/tabs-ctrl.js
+++ app/main/controllers/tabs-ctrl.js
@@ -1,14 +1,19 @@
 'use strict';
 angular.module('main')
-.controller('TabsCtrl', [ '$scope', 'Patients', function ($scope, Patients) {
+.controller('TabsCtrl', [ '$scope', '$state', function ($scope, $state) {
   console.log('Hello from your Controller: TabsCtrl in module main:. This is your controller:', this);
 
   var ctrl = this;
 
-  ctrl.patient = Patients.patient;
-  $scope.$on('patient.update', function () {
-    console.log('TabsCrtl got patient update' + Patients.patient);
-    ctrl.patient = Patients.patient;
-    $scope.$apply();
+  $scope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+  	console.log("TabsCtrl: Checking state");
+  	if (toState.name.indexOf('tabs') == 0){
+  		console.log('TabsCtrl: State passed');
+  		return true;
+  	}
+  	event.preventDefault();
+  	console.log("TabsCtrl: Editing state to: " + 'tabs.' + toState.name);
+  	$state.go('tabs.' + toState.name, toParams);
   });
+
 }]);
