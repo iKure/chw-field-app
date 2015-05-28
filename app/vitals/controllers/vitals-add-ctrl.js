f8ae8a33922f6a38a8eb7f23e4785b743bcaa5f9
Nick Reid <nickreid@nickreid.com>
2015-05-28 10:44:17 +0530

    made currentSet status unique to controller

--- app/vitals/controllers/vitals-add-ctrl.js
+++ app/vitals/controllers/vitals-add-ctrl.js
@@ -3,12 +3,14 @@ angular.module('vitals')
 .controller('VitalsAddCtrl', [ '$scope', 'Vitals', function ($scope, Vitals) {
   console.log('Hello from your Controller: VitalsCtrl in module vitals:. This is your controller:', this);
   var ctrl = this;
-  Vitals.clearCurrentSet();
   ctrl.currentSet = {};
 
-  $scope.$on('vitals.update', function () {
-    ctrl.currentSet = Vitals.getCurrentSet();
-    $scope.$apply();
+  $scope.$on('vitals.change', function () {
+    var current = Vitals.getCurrent();
+    if (!current) {
+      return false;
+    }
+    ctrl.currentSet[current.field_type] = current._id;
   });
 
   function isSaved (type) {
