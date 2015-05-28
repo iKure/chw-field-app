7a8270670a02f1ca6cd404ef1307a45710822d3b
Nick Reid <nickreid@nickreid.com>
2015-05-28 12:07:56 +0530

    updating Patients Module to use shared services

--- app/patients/controllers/directory-ctrl.js
+++ app/patients/controllers/directory-ctrl.js
@@ -3,11 +3,10 @@ angular.module('patients')
 .controller('DirectoryCtrl', [ '$scope', 'Patients', function ($scope, Patients) {
   console.log('Hello from your Controller: DirectoryCtrl in module patients:. This is your controller:', this);
   var directory = this;
-  directory.patients = Patients.patients;
+  directory.patients = Patients.get();
 
   $scope.$on('patients.update', function () {
-    directory.patients = Patients.patients;
+    directory.patients = Patients.get();
     $scope.$apply();
-    console.log('DirectoryCtrl got patients update' + Patients.patients);
   });
 }]);
--- app/patients/services/patients-serv.js
+++ app/patients/services/patients-serv.js
@@ -3,61 +3,14 @@ angular.module('patients')
 .service('Patients', [
   '$rootScope',
   'Config',
-  function ($rootScope, Config) {
+  'SharedState',
+  function ($rootScope, Config, SharedState) {
     console.log('Hello from your Service: Patients in module patients');
 
-    var localDB = new PouchDB('patients');
 
-    if (Config.ENV.PATIENTS) {
-      Config.ENV.PATIENTS.forEach( function (patient) {
-        localDB.get(patient._id).catch(function () {
-          if (patient._id) {
-            localDB.put(patient);
-          }
-        });
-      });
-    }
+    var service = SharedState.makeService('patients');
 
-    var service = {
-      patient: false,
-      getPatients:function () {
-        console.log('PatientService: fetching all patients');
-        localDB.allDocs({include_docs: true, descending: true}, function (err, docs) {
-          service.patients = [];
-          docs.rows.forEach( function (row) {
-            service.patients.push(row.doc);
-          });
-          $rootScope.$broadcast('patients.update');
-        });
-      },
-      addPatient: function (patient) {
-        console.log('got patient: ' + patient.firstName);
-        return localDB.post(patient).then(function (response) {
-          console.log('Service made new id:' + response.id);
-          service.getPatients();
-          $rootScope.$broadcast('patient.new', response.id);
-        });
-      },
-      savePatient: function (patient) {
-        console.log('patients-serv: SAVING PATIENT ' + patient.firstName);
-        return localDB.put(patient).then(function (response) {
-          service.getPatients();
-          service.getPatient(response.id);
-        });
-      },
-      getPatient: function (id) {
-        console.log('Getting patient: ' + id);
-        localDB.get(id).then( function (doc) {
-          service.patient = doc;
-          $rootScope.$broadcast('patient.update');
-        }).catch( function () {
-          $rootScope.$broadcast('patient.update');
-        });
-        return false;
-      }
-    };
-
-    service.getPatients();
+    service.updateRecords();
 
     return service;
   }
