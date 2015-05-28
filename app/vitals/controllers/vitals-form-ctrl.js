4af2002314b1aae492282cb304760634b8cda472
Nick Reid <nickreid@nickreid.com>
2015-05-28 10:38:39 +0530

    cleaned up VitalsFormCtrl

--- app/vitals/controllers/vitals-form-ctrl.js
+++ app/vitals/controllers/vitals-form-ctrl.js
@@ -1,27 +1,24 @@
 'use strict';
 angular.module('vitals')
-.controller('VitalsFormCtrl', [ '$scope', '$state', '$stateParams', 'Patients', 'Vitals', function ($scope, $state, $stateParams, Patients, Vitals) {
-  console.log('Hello from your Controller: VitalsAddFormCtrl in module vitals:. This is your controller:', this);
+.controller('VitalsFormCtrl', [ '$scope', '$state', '$stateParams', 'Vitals', function ($scope, $state, $stateParams, Vitals) {
+  console.log('Hello from your Controller: VitalsFormCtrl in module vitals:. This is your controller:', this);
   var ctrl = this;
-  this.type = $stateParams.type;
+  ctrl.type = $stateParams.type;
 
-  this.current = {};
-  var currentID = Vitals.getCurrentSet(this.type);
-  if (currentID) {
-    Vitals.getById(currentID).then(function (doc) {
-      ctrl.current = doc;
-    });
+  ctrl.current = {};
+  if($stateParams.vitals_id) {
+    Vitals.getById($stateParams.vitals_id);
   }
 
   this.saveRecord = function (record) {
-    record.type = ctrl.type;
+    if(!record.field_type){
+      record.field_type = ctrl.type;
+    }
     Vitals.save(record);
   }
 
-  $scope.$on('vitals.current.update', function () {
-    if (Vitals.current.type == ctrl.type) {
-      ctrl.current = Vitals.current;
-      $scope.$apply();
-    }
+  $scope.$on('vitals.change', function () {
+    ctrl.current = Vitals.getCurrent();
+    $scope.$apply();
   });
 }]);
