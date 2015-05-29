1a8ad0b107bb297674755e6edd55ce6367d7d6e4
Nick Reid <nickreid@nickreid.com>
2015-05-29 09:20:54 +0530

    fixed loosing state in non-paramed states

--- app/patients/controllers/profile-ctrl.js
+++ app/patients/controllers/profile-ctrl.js
@@ -2,11 +2,15 @@
 angular.module('patients')
 .controller('ProfileCtrl', [ '$scope', '$stateParams', 'Patients', function ($scope, $stateParams, Patients) {
   console.log('Hello from your Controller: ProfileCtrl in module patients:. This is your controller:', this);
-  var profile = this;
 
-  Patients.setCurrent($stateParams.id);
+  if (Patients.getCurrent()) {
+    $scope.patient = Patients.getCurrent();
+  }
+  if ($stateParams.id) {
+    Patients.setCurrent($stateParams.id);
+  }
   $scope.$on('patient.change', function () {
-    profile.patient = Patients.getCurrent();
+    $scope.patient = Patients.getCurrent();
     $scope.$apply();
   });
 }]);
