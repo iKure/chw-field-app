db0934201063715563db29f19b5021be1807a420
Nick Reid <nickreid@nickreid.com>
2015-06-07 15:56:37 +0530

    added boiler plate route structure

--- app/forms/forms.js
+++ app/forms/forms.js
@@ -3,18 +3,24 @@ angular.module('forms', [
   'ionic',
   'ngCordova',
   'ui.router',
-  // TODO: load other modules selected during generation
+  'shared',
 ])
 .config(function ($stateProvider) {
 
   console.log('Allo! Allo from your module: ' + 'forms');
 
-  // some basic routing
   $stateProvider
     .state('forms', {
       url: '/forms',
       templateUrl: 'forms/templates/start.html',
       controller: 'StartCtrl as start'
+    })
+    .state('forms.create', {
+      url: '/new/:type',
+      templateUrl: 'forms/templates/start.html',
+    })
+    .state('forms.view', {
+      url: '/:form_id',
+      templateUrl: 'forms/templates/start.html',
     });
-  // TODO: do your thing
 });
