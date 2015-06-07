0e4a7b70fed54259c21ba87e7429085d2bd4de24
Nick Reid <nickreid@nickreid.com>
2015-06-07 22:57:12 +0530

    changed odk-list to forms-list

--- app/forms/forms.js
+++ app/forms/forms.js
@@ -19,7 +19,7 @@ angular.module('forms', [
       url: '/list',
       views:{
         'formsContent':{
-          templateUrl: 'forms/templates/odk-list.html',
+          templateUrl: 'forms/templates/forms-list.html',
           controller: 'OdkCtrl as Odk'
         }
       }
