a1427a824acf8d420247538d2709774e196b4e3a
Nick Reid <nickreid@nickreid.com>
2015-06-09 23:25:00 +0530

    changed layout a little

--- app/patients/controllers/directory-ctrl.js
+++ app/patients/controllers/directory-ctrl.js
@@ -1,12 +1,14 @@
 'use strict';
 angular.module('patients')
 .controller('PatientDirectoryCtrl', [ '$scope', 'Patients', function ($scope, Patients) {
-  console.log('Hello from your Controller: DirectoryCtrl in module patients:. This is your controller:', this);
+  console.log('Hello from your Controller: PatientDirectoryCtrl in module patients:. This is your controller:', this);
   $scope.records = [];
   Patients.list().then(function (results) {
+    console.log('PatientDirectoryCtrl: Got ' + results.rows.length + ' patients');
     $scope.records = [];
     results.rows.forEach(function (row) {
       $scope.records.push(row.doc);
     });
+    $scope.$apply();
   });
 }]);
