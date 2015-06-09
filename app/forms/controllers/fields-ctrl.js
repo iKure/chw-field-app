1f1d6db76a1b31e9fa54f626fd7ec49ea11a9b13
Nick Reid <nickreid@nickreid.com>
2015-06-09 09:01:04 +0530

    got values to save with parent form

--- app/forms/controllers/fields-ctrl.js
+++ app/forms/controllers/fields-ctrl.js
@@ -1,44 +1,20 @@
 'use strict';
 angular.module('forms')
-.controller('FieldsCtrl', ['$scope', '$state', '$stateParams', 'Fields', 'Forms', function ($scope, $state, $stateParams, Fields, Forms) {
+.controller('FieldCtrl', ['$scope', 'Fields', 'Forms', function ($scope, Fields) {
 
-  console.log('Hello from your Controller: FieldsCtrl in module forms:. This is your controller:', this);
+  console.log('Hello from your Controller: FieldCtrl in module forms:. This is your controller:', this);
 
-  function redirect (reason) {
-    if (!reason) {
-      reason = "??";
-    }
-    console.log('FieldsCtrl: ' + reason + ' —> redirecting');
-    $state.go('forms.list');
-  }
-  if (!$stateParams.field_id) {
-    redirect('No field_id');
-  }
-
-  $scope.field_id = $stateParams.field_id;
-  $scope.data = {};
-  $scope.fields = [];
-
-  Fields.get($stateParams.field_id).then(function (doc) {
-    console.log('FieldsCtrl: Got field data: ' + doc._id);
-    $scope.data = doc;
-    console.log('FieldsCtrl: Getting form: ' + doc.form_id);
-    Forms.get(doc.form_id).then(function (doc) {
-      console.log('FieldsCtrl: Got form structure: ' + doc._id);
-      $scope.fields = doc.fields;
-      $scope.$apply();
-    });
+  $scope.$on('field.save', function (event, field_id) {
+    console.log("FieldCtrl: Caught field save event — " + field_id + ' -> ' + $scope.field.name);
+    $scope.data.parent = field_id;
+    save();
   });
-
-  function update (key, value) {
-    $scope.data[key] = value;
-  }
-  $scope.update = update;
 
   function save () {
     console.log('FieldsCtrl: Saving field: ' + $scope.data._id);
-    Fields.save($scope.data).then(function (response) {
+    Fields.save(data).then(function (response) {
       console.log('FieldsCtrl: Saved field: ' + response.id);
+      $scope.data._id = response.id;
       $scope.data._rev = response.rev;
     });
   }
--- app/forms/directives/field-form-dir.js
+++ app/forms/directives/field-form-dir.js
@@ -5,18 +5,24 @@ angular.module('forms')
     templateUrl: 'forms/templates/field-form.html',
     restrict: 'A',
     link: function postLink (scope, element, attrs) {
-      console.log("fieldFormDirective: Yo! Check this " + scope.field.type);
+      var type = scope.field.type;
+      if (!scope.data) {
+        scope.data = {};
+      }
+      console.log("fieldFormDirective: Yo! Check this " + type);
       if (!scope.data) {
         console.log("fieldFormDirective: No data");
       }
-      scope.contentUrl = 'forms/templates/field-' + scope.field.type + '-form.html';
+      scope.contentUrl = 'forms/templates/field-' + type + '-form.html';
     },
     scope: {
+      save: '&onSave',
       update: '&onUpdate',
       close: '&onClose',
       field_id: '=fieldId',
       field: '=field',
       data: '=data'
-    }
+    },
+    controller: 'FieldCtrl',
   };
 });
