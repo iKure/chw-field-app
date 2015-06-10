f490b9ca1b27a0e941219d479d95db81ecb80a41
Nick Reid <nickreid@nickreid.com>
2015-06-10 13:23:05 +0530

    got keyboard to open after modal opens

--- app/forms/controllers/field-text-edit-ctrl.js
+++ app/forms/controllers/field-text-edit-ctrl.js
@@ -1,6 +1,6 @@
 'use strict';
 angular.module('forms')
-.controller('FieldTextEditCtrl', ['$scope', '$ionicModal', function ($scope, $ionicModal) {
+.controller('FieldTextEditCtrl', ['$scope', '$ionicModal', '$cordovaKeyboard', function ($scope, $ionicModal, $cordovaKeyboard) {
 
   console.log('Hello from your Controller: FieldTextEditCtrl in module forms:. This is your controller:', this);
   $ionicModal.fromTemplateUrl('forms/templates/field-text-edit-modal.html', {
@@ -9,13 +9,22 @@ angular.module('forms')
     focusFirstInput: true,
   }).then(function (modal) {
     $scope.modal = modal;
+    console.log("Modal created for " + $scope.label);
   });
+  console.log('Should have created a modal!');
+
   $scope.openModal = function () {
-    $scope.modal.show();
+    $scope.modal.show().then(function () {
+      $cordovaKeyboard.show();
+      console.log("Modal opened for " + $scope.label);
+    });
   };
+
   $scope.closeModal = function () {
+    $cordovaKeyboard.close();
     $scope.modal.hide();
   };
+
   $scope.$on('$destroy', function () {
     $scope.modal.remove();
   });
