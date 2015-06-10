9c58912c8faadaad1604773097f1892861e9b3cd
Nick Reid <nickreid@nickreid.com>
2015-06-10 23:57:54 +0530

    added form-item-view

--- app/patients/controllers/patient-summary-ctrl.js
+++ app/patients/controllers/patient-summary-ctrl.js
@@ -4,16 +4,13 @@ angular.module('patients')
 
   console.log('Hello from your Controller: PatientSummaryCtrl in module patients:. This is your controller:', this);
 
+  $scope.records = null;
   Patients.get($stateParams.patient_id).then( function (doc) {
     $scope.patient = doc;
     Fields.all({
       patient_id: $scope.patient._id,
     }).then(function (results) {
-      $scope.records = [];
-      results.rows.forEach(function (row) {
-        $scope.records.push(row.doc);
-      });
-      $scope.$apply();
+      $scope.records = results;
     });
   });
 }]);
