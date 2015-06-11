0b18b25ed1b6d3bbf7ddfcd8f1ec48c08406326a
Nick Reid <nickreid@nickreid.com>
2015-06-11 22:47:25 +0530

    changed location of save button and save function placement

--- app/forms/controllers/form-ctrl.js
+++ app/forms/controllers/form-ctrl.js
@@ -48,4 +48,51 @@ angular.module('forms')
     });
   }
 
+  function save() {
+    var timeout = false;
+    var saving = [];
+    // Set listeners to check for saved events
+    $scope.$on('field.saving', function (event, name) {
+      console.log("FormCtrl: Got saving from: " + name);
+      saving.push(name);
+    });
+    $scope.$on('field.saved', function (event, name) {
+      console.log("FormCtrl: " + name + ": is saved");
+      saving.splice(saving.indexOf(name), 1);
+    });
+
+    function close () {
+      if (saving.length > 0) {
+        console.log('FormCtrl: Waiting for ' + saving);
+        closeTimeout(100);
+        return false;
+      }
+      console.log('FormCtrl: Closing');
+      if ($scope.close) {
+        $scope.close();
+      }
+    }
+    function closeTimeout (duration) {
+      console.log('FormCtrl: Set close timeout: ' + duration);
+      return setTimeout(close, duration);
+    }
+
+    if ($scope.form) {
+      $scope.data.form_id = $scope.form._id;
+      $scope.data.form_label = $scope.form.label;
+    }
+
+    if ($scope.label) {
+      $scope.data.form_label = $scope.label;
+    }
+
+    Fields.save($scope.data).then(function (response) {
+      $scope.data._id = response.id;
+      $scope.data._rev = response.rev;
+      $scope.$broadcast('field.save', response.id);
+      closeTimeout(100);
+    });
+  }
+  $scope.save = save;
+
 }]);
