b674091e755b20a8debd2f81053a2ba93467fb3a
Nick Reid <nickreid@nickreid.com>
2015-06-14 16:09:51 +0530

    added in Pouch-auth functions

--- app/auth/services/auth-serv.js
+++ app/auth/services/auth-serv.js
@@ -2,17 +2,32 @@
 angular.module('auth')
 .service('Auth', ['$q','$rootScope', 'Config', 'pouchDB', function ($q, $rootScope, Config, pouchDB) {
   console.log('Hello from your Service: Auth in module auth');
-  var localDB = new pouchDB('auth');
+  
+  if (Config.ENV.SERVER_URL) {
+  	var local = new pouchDB('auth');
+  	var db = new PouchDB(Config.ENV.SERVER_URL + 'auth');
+  	local.sync(db, {live: true, retry: true}).on('error', console.log.bind(console));
+  }
 
   var service = {};
 
   function getSession () {
   	var deferred = $q.defer();
-  	console.log(Config.ENV);
   	if (Config.ENV.SESSION) {
   		deferred.resolve(Config.ENV.SESSION);
   	} else if (Config.ENV.SERVER_URL) {
   		console.log('AuthServ: Checking SERVER_URL');
+		db.getSession(function (err, response) {
+		  if (err) {
+		    console.log("AuthServ: Could not reach network");
+		  } else if (!response.userCtx.name) {
+		    console.log("AuthServ: User not logged in!");
+		    deferred.reject(false);
+		  } else{
+		  	console.log("AuthServ: Got a user");
+		  	deferred.resolve(response);
+		  }
+		});
   	} else {
   		deferred.reject(false);
   	}
