db0c7abf413cd75374692e4d2c387a4f3ff8d433
Nick Reid <nickreid@nickreid.com>
2015-06-15 11:33:58 +0530

    changed save method for patient form to get rid of accidental patient creation

--- app/patients/controllers/patient-form-ctrl.js
+++ app/patients/controllers/patient-form-ctrl.js
@@ -7,10 +7,19 @@ angular.module('patients')
   $scope.data = {};
   Forms.get('demographics').then(function (doc) { // This should be configured in a seeting somewhere
     $scope.form = doc;
+  }).catch(function (err) {
+    console.error(err);
+    // Show error message?
+    $state.go('patients.directory');
   });
 
   function save() {
     var patient = {};
+
+    if (!$scope.data._id) { // if form was not saved, just close
+      close();
+      return false;
+    }
 
     $scope.form.fields.forEach( function (field) {
       if (!field.persistant || !$scope.data[field.name]) {
@@ -24,9 +33,18 @@ angular.module('patients')
     Patients.save(patient).then(function (patient) {
       $scope.data.patient_id = patient._id;
       Fields.save($scope.data).then(function () {
-        $state.go('patients.single.summary', { patient_id:patient._id });
+        close();
       });
     });
   }
   $scope.save = save;
+
+  function close () {
+    if ($scope.data.patient_id) {
+      $state.go('patients.single.summary', { patient_id:$scope.data.patient_id });
+    } else {
+      $state.go('patients.directory');
+    }
+  }
+  $scope.close = close;
 }]);
