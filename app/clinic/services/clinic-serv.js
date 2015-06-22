337fb4d6c1d1fb6ddb84f5f9db2cc5fc9df24d4a
Nick Reid <nickreid@nickreid.com>
2015-06-22 17:51:00 +0530

    added skeleton of clinic code

--- app/clinic/services/clinic-serv.js
+++ app/clinic/services/clinic-serv.js
@@ -1,7 +1,27 @@
 'use strict';
 angular.module('clinic')
-.service('Clinic', function () {
+.service('Clinic', ['$rootScope', '$q', 'Config', 'pouchDB', 'Auth', function ($rootScope, $q, Config, pouchDB, Auth) {
   console.log('Hello from your Service: Clinic in module clinic');
 
-  // TODO: do your service thing
-});
+  var service = {}
+  // List available clinics — based on User's permission to databases
+  function getClinics(user) {
+    return [];
+  }
+  service.getClinics = getClinics;
+  // Set pouch to load specific clinic
+  function setClinic(clinic_id) {
+    // Create or get pouch instance
+    // attempt to set up syncing
+    return true;
+  }
+  service.setClinic = setClinic;
+
+  function sync() {
+    // cancel current syncing (if exists)
+    // create new live environment
+    return true;
+  }
+
+  return service;
+}]);
