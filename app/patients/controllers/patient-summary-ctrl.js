2af04fcf401ac2c81123db591613feb288b3b343
Nick Reid <nickreid@nickreid.com>
2015-06-23 09:19:55 +0530

    added hook to catch field records update

--- app/patients/controllers/patient-summary-ctrl.js
+++ app/patients/controllers/patient-summary-ctrl.js
@@ -1,8 +1,16 @@
 'use strict';
 angular.module('patients')
-.controller('PatientSummaryCtrl', ['$scope', 'records', function ($scope, records) {
+.controller('PatientSummaryCtrl', ['$scope', 'Fields', 'patient', 'records', function ($scope, Fields, patient, records) {
 
   console.log('Hello from your Controller: PatientSummaryCtrl in module patients:. This is your controller:', this);
   console.log ("Hello RECORDS" + records);
   $scope.records = records;
+
+  $scope.$on('clinic.update', function () {
+    Fields.all({
+      patient_id:patient._id
+    }).then(function (results) {
+      $scope.records = results;
+    });
+  });
 }]);
