f3c93f4e5d06805e136e6821e8b296913da3581f
Nick Reid <nickreid@nickreid.com>
2015-06-24 18:57:47 +0530

    used directive to contain input type logic

--- app/forms/directives/field-form-dir.js
+++ app/forms/directives/field-form-dir.js
@@ -1,28 +1,20 @@
 'use strict';
 angular.module('forms')
-.directive('fieldForm', function () {
+.directive('fieldInput', function () {
   return {
-    templateUrl: 'forms/templates/field-form.html',
+    templateUrl: 'forms/templates/input-base.html',
     restrict: 'A',
     link: function postLink (scope, element, attrs) {
       var type = scope.field.type;
-      if (!scope.data) {
-        scope.data = {};
-      }
       console.log("fieldFormDirective: Yo! Check this " + type);
       if (!scope.data) {
         console.log("fieldFormDirective: No data");
       }
-      scope.contentUrl = 'forms/templates/field-' + type + '-form.html';
+      scope.contentUrl = 'forms/templates/input-' + type + '.html';
     },
     scope: {
-      save: '&onSave',
-      update: '&onUpdate',
-      close: '&onClose',
-      field_id: '=fieldId',
       field: '=field',
       data: '=data'
-    },
-    controller: 'FieldCtrl',
+    }
   };
 });
