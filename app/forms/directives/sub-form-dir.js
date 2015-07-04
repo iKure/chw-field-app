466d631fa611bec095d497472a749e4586497fea
Nick Reid <nickreid@nickreid.com>
2015-07-04 11:30:43 +0530

    added in simple version of slider with ng-pageslide

--- app/forms/directives/sub-form-dir.js
+++ app/forms/directives/sub-form-dir.js
@@ -1,27 +1,19 @@
 'use strict';
 angular.module('forms')
-.directive('subForm', ['$q', '$compile', function ($q, $compile) {
+.directive('subForm', ['$ionicBackdrop', '$q', '$compile', function ($ionicBackdrop, $q, $compile) {
   return {
     restrict: 'E',
     transclude: true,
-    template: '<div ng-transclude></div>',
+    templateUrl: 'forms/templates/sub-form.html',
     link: function (scope, element, attr) {
       console.log('SubForm: Yo!');
-      var div = false;
-      element.hide();
       scope.$watch('showing', function (newValue) {
         if (newValue) {
           console.log("SubForm: showing!!");
-          var parent = element.parents('ion-content:first');
-          div = $($compile('<ion-content></ion-content>')(scope)).insertAfter(parent);
-          div.attr('class', parent.attr('class'));
-          div.css('left', parent.width());
-          $('.scroll', div).append(element);
+          $ionicBackdrop.retain();
         } else {
-          if (div) {
-            div.remove();
-            div = false;
-          }
+          console.log("SubForm: HIDE!!");
+          $ionicBackdrop.release();
         }
       });
     }
